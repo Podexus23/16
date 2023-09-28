b48853e5df9e75fcd55aa17ea39d59eb6c5f88ca
Podexus23 <santon23@gmail.com>
2023-09-28 14:01:54 +0300

    feat: add mongoose and database

--- server.js
+++ server.js
@@ -1,7 +1,54 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+    console.log(`doc saved`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then(() => console.log('DB connection successful'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
