41a0bfd2d6124e90d785b00913f1d546ece42599
Podexus23 <santon23@gmail.com>
2023-10-02 22:33:55 +0300

    feat: monthly aggregator

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -175,3 +175,55 @@ exports.getTourStats = async (req, res) => {
     });
   }
 };
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = +req.params.year;
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: `$startDates` },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $sort: {
+          numTourStarts: -1,
+        },
+      },
+      {
+        $project: { _id: 0 },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        length: plan.length,
+        plan,
+      },
+      message: 'hi unwind',
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
