121051ef0fb60cea5a293b0b8f2ee457d42e7a34
Podexus23 <santon23@gmail.com>
2023-10-20 15:39:14 +0300

    feat: add some security

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,10 +15,44 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //1) Middlewares
+
+// security HTTP headers
+app.use(helmet());
+// development logi
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(express.json());
+// limit requests from same IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
+// body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+// data sanitization against XSS
+app.use(xss());
+
+//prevent parameter polution
+app.use(
+  hpp({
+    whiteList: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
+// serving static files
 app.use(express.static(`${__dirname}\\public`));
 
 app.use((req, res, next) => {
