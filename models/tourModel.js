a3e584e2ca8d5750007fa155215451358f3ecf42
Podexus23 <santon23@gmail.com>
2023-10-30 20:54:40 +0300

    feat: add review population

--- models/tourModel.js
+++ models/tourModel.js
@@ -120,6 +120,13 @@ tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
 
+// virtual populate
+tourSchema.virtual('reviews', {
+  ref: 'Review',
+  foreignField: 'tour',
+  localField: '_id',
+});
+
 // Document middleware: runs before save() and .create()
 tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
@@ -162,7 +169,7 @@ tourSchema.post(/^find/, function (docs, next) {
 
 // Aggregation middleware
 tourSchema.pre('aggregate', function (next) {
-  console.log(this.pipeline());
+  // console.log(this.pipeline());
   this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
   next();
 });
