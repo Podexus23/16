ad908563d4804695a114f278ddd7461f882e7e3a
Podexus23 <santon23@gmail.com>
2023-11-01 23:55:03 +0300

    feat: add geo calculation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
 // const APIFeatures = require('../utils/apiFeatures');
@@ -93,6 +94,32 @@ exports.getMonthlyPlan = catchAsync(async (req, res, next) => {
     data: {
       length: plan.length,
       plan,
+    },
+  });
+});
+
+exports.getTourWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  if (!lat || !lng)
+    next(
+      new AppError(
+        'Please provide latitude amd longitude in the format lat,lng.',
+      ),
+    );
+
+  const tours = await Tour.find({
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      data: tours,
     },
   });
 });
