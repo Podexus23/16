80853e3aff9ec7c675d1866f18079aa701311896
Podexus23 <santon23@gmail.com>
2023-11-02 23:47:28 +0300

    feat: add blocks and controllrs for pages

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
+const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
 
@@ -65,13 +66,8 @@ app.use((req, res, next) => {
 });
 
 // 3) Routes
-app.get('/', (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Foresst Hiker',
-    user: 'Jonas',
-  });
-});
 
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
