907b51af2452d4efddfb5790ffde11d21bfe8de3
Podexus23 <santon23@gmail.com>
2023-11-03 17:24:35 +0300

    feat: our page 1/2

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,11 +1,29 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  //1) Get tour data from collection
+  const tours = await Tour.find();
+  //2) Build template
+  //3) Render that template useing tour data from 1
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'the Forest Hiker Tour',
+exports.getTour = catchAsync(async (req, res) => {
+  //1) get the data, for he requested tour(including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user',
   });
-};
+  //2) build template
+  //3) render template using data
+
+  res.status(200).render('tour', {
+    title: `${tour.name} tour`,
+    tour,
+  });
+});
