85bb570d4dc5879302f7b664e3ff662d46ed3eef
Podexus23 <santon23@gmail.com>
2023-11-04 20:07:12 +0300

    feat: add login

--- controllers/authController.js
+++ controllers/authController.js
@@ -66,6 +66,32 @@ exports.login = catchAsync(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
+// only for rendered pages, no errors!
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+  if (req.cookies.jwt) {
+    // verification token
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET,
+    );
+
+    // 2) Check  if user changed password after the token was issued
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next();
+    }
+
+    // 3) Check  if user still exists
+    if (!currentUser.changedPasswordAfter(decoded.iat)) {
+      return next();
+    }
+    // there is a logged in a user
+    res.locals.user = currentUser;
+    return next();
+  }
+  next();
+});
+
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting the token and check if it's there
   let token;
@@ -75,6 +101,8 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
 
   if (!token) {
